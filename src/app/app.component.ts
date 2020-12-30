abb3cc15f19553bd079f44fe17da0f5417fef6a7
Lincoln Karuhanga <lincoln.karuhanga@gmail.com>
2020-12-30 06:20:12 +0300

    auth.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {AuthService} from "./auth.service";
+import {Router} from "@angular/router";
+import {Subscription} from "rxjs";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
   title = 'pencil-frontend';
+  userSubjectSubscription?: Subscription;
+
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+  ) { }
+
+  ngOnInit() {
+    this.userSubjectSubscription = this.authService.userSubject.subscribe(user => {
+      if (user) this.router.navigate(['']);
+      else this.router.navigate(['login']);
+    })
+  }
+
+  ngOnDestroy() {
+    this.userSubjectSubscription && this.userSubjectSubscription.unsubscribe();
+  }
 }
